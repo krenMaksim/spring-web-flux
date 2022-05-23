98d993fa76e3b3e0545d12ebda1bdf1d14bf1e18
Maksim Kren <maksim_kren@epam.com>
2022-05-23 18:04:47 +0300

    add someting working

--- simple-config/src/main/java/com/kren/spring/web/flux/app/Application.java
+++ simple-config/src/main/java/com/kren/spring/web/flux/app/Application.java
@@ -1,7 +1,33 @@
 package com.kren.spring.web.flux.app;
 
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.server.HttpServer;
+
 public class Application {
 
 	public static void main(String[] args) {
+
+        HttpHandler handler = new HttpHandler() {
+
+            @Override
+            public Mono<Void> handle(ServerHttpRequest request, ServerHttpResponse response) {
+                return Mono.fromRunnable(() -> {
+                    System.out.println("DO NOTHING");
+                });
+            }
+        };
+        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
+        HttpServer.create()
+            .host("localhost")
+            .port(8080)
+            .handle(adapter)
+            .bind()
+            .block();
+
+        System.out.println("done");
 	}
 }
