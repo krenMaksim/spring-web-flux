2063e7d20eaa3ddfee5303c8bb3a440ca4567dc3
krenMaksim <krenmaba@gmail.com>
2022-05-24 08:03:04 +0300

    refactring Application

--- simple-config/src/main/java/com/kren/spring/web/flux/app/Application.java
+++ simple-config/src/main/java/com/kren/spring/web/flux/app/Application.java
@@ -5,28 +5,27 @@ import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import reactor.core.publisher.Mono;
-import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-        HttpHandler handler = new HttpHandler() {
+        var handler = new HttpHandler() {
             @Override
             public Mono<Void> handle(ServerHttpRequest request, ServerHttpResponse response) {
                 return Mono.fromRunnable(() -> {
                     System.out.println("DO NOTHING");
+                    System.out.println(request);
+                    System.out.println(response);
                 });
             }
         };
 
-        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
-
-        DisposableServer server = HttpServer.create()
+        var server = HttpServer.create()
             .host("localhost")
             .port(8080)
-            .handle(adapter)
+            .handle(new ReactorHttpHandlerAdapter(handler))
             .bindNow();
 
         server.onDispose()
