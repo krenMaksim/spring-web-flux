e3bb70c73c93e1fa3b6cd67de0642d673a0af9eb
krenMaksim <krenmaba@gmail.com>
2022-05-24 09:00:10 +0300

    done EnableWebFluxApp

--- simple-config/src/main/java/com/kren/spring/web/flux/app/EnableWebFluxApp.java
+++ simple-config/src/main/java/com/kren/spring/web/flux/app/EnableWebFluxApp.java
@@ -1,15 +1,25 @@
 package com.kren.spring.web.flux.app;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServer;
 
-public class EnableWebFluxApp {
+@Configuration
+@EnableWebFlux
+class EnableWebFluxApp implements WebFluxConfigurer {
 
 	public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
+        var context = new AnnotationConfigApplicationContext(EnableWebFluxApp.class);
 
         var handler = WebHttpHandlerBuilder.applicationContext(context)
             .build();
@@ -23,5 +33,17 @@ public class EnableWebFluxApp {
         server.onDispose()
             .block();
 	}
+
+    @Component
+    static class CustomWebFilter implements WebFilter {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+            return Mono.fromRunnable(() -> {
+                System.out.println("Invoked: " + this.getClass().getSimpleName());
+            });
+        }
+
+    }
 
 }
