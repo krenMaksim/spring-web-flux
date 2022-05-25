d4dff6b381578077ce7d7efe717a51c25e1500cf
krenMaksim <krenmaba@gmail.com>
2022-05-25 08:47:41 +0300

    added simple-spring-boot-config

---
+++ simple-spring-boot-config/src/main/java/com/kren/spring/web/flux/app/SpringApp.java
@@ -0,0 +1,13 @@
+package com.kren.spring.web.flux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringApp {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+	}
+
+}
--- simple-spring-boot-config/src/main/java/com/kren/spring/web/flux/app/config/RoutingConfig.java
+++ simple-spring-boot-config/src/main/java/com/kren/spring/web/flux/app/config/RoutingConfig.java
@@ -2,10 +2,8 @@ package com.kren.spring.web.flux.app.config;
 
 import com.kren.spring.web.flux.app.handler.PersonHandler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,10 +11,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-@EnableWebFlux
-@Configuration
-@ComponentScan("com.kren.spring.web.flux.app.handler")
-public class SpringWebFluxConfig { // in case it is needed can implement WebFluxConfigurer
+@Configuration(proxyBeanMethods = false)
+public class RoutingConfig {
 
     private static final RequestPredicate ACCEPT_JSON = accept(MediaType.APPLICATION_JSON);
 
