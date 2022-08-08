2a6e82acc5fb674664da1c22ade33663535dbe64
krenMaksim <krenmaba@gmail.com>
2022-08-08 17:03:06 +0300

    added initial commit

---
+++ own-publisher-subscriber/src/main/java/com/kren/spring/web/flux/app/publisher/PersonHandler.java
@@ -0,0 +1,22 @@
+package com.kren.spring.web.flux.app.publisher;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PersonHandler {
+
+    public Mono<ServerResponse> listPeople(ServerRequest request) {
+        return Mono.fromRunnable(() -> System.out.println("Requested listPeople"));
+    }
+
+    public Mono<ServerResponse> createPerson(ServerRequest request) {
+        return Mono.fromRunnable(() -> System.out.println("Requested createPerson"));
+    }
+
+    public Mono<ServerResponse> getPerson(ServerRequest request) {
+        return Mono.fromRunnable(() -> System.out.println("Requested getPerson"));
+    }
+}
