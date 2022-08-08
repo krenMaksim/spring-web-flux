315d4518a12f7796d46549faf13240440bd04c7a
krenMaksim <krenmaba@gmail.com>
2022-08-08 17:55:56 +0300

    added LettersSubscriberTest

---
+++ own-publisher-subscriber/src/test/java/LettersSubscriberTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class LettersSubscriberTest {
+
+    @Test
+    void doSomething() {
+        fail("TODO");
+    }
+
+}
