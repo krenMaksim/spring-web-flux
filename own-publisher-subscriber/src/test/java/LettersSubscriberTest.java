68f20a55ee779640fc32259d7c4c12271b043ff0
krenMaksim <krenmaba@gmail.com>
2022-08-09 10:36:07 +0300

    added publisher

--- own-publisher-subscriber/src/test/java/LettersSubscriberTest.java
+++ own-publisher-subscriber/src/test/java/LettersSubscriberTest.java
@@ -1,12 +1,22 @@
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.fail;
+import reactor.core.publisher.Flux;
 
 class LettersSubscriberTest {
 
+    // See link https://www.appsdeveloperblog.com/reactive-programming-creating-publishers-and-subscribers-in-java/
+    /*
+        We will implement a Subscriber Class ourselves.
+        This Subscriber will receive the small letters from the Publisher 
+        to which it subscribed, convert them to capital letters, and print them out. 
+    */
+
+
     @Test
     void doSomething() {
-        fail("TODO");
+        var publisher = Flux.just('a', 'b', 'c', 'd', 'e', 'f', 'g');
+
+        publisher.log()
+            .blockLast();
     }
 
 }
